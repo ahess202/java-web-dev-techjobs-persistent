b850d7299348d75ee98c046a36ea7ca3672877b2
ahess202 <anthony_hess@comcast.com>
2020-05-26 15:18:36 -0400

    implemented the add job functionality, as well as listing the jobs at the index

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -5,7 +5,10 @@ import javax.persistence.*;
 @Entity
 public class Job extends AbstractEntity{
 
+    @ManyToOne
     private Employer employer;
+
+
     private String skills;
 
     public Job() {
