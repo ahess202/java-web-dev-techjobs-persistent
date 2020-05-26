b457bf3a9a7dfa7de54704c0cb4bbb0532eb23ef
ahess202 <anthony_hess@comcast.com>
2020-05-26 11:26:11 -0400

    finished building the employer and skill classes, as well as their repository interfaces

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Must enter an employer!")
+    private String location;
 
+    public Employer () {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
